64b98e5db1effdf5980af0f8ae41be854fb50dfa
Khalid Danishyar <danishyarkhwork@gmail.com>
2023-09-17 22:17:57 +0430

    Chat UI Created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Events\SendMessage;
+use BeyondCode\LaravelWebSockets\Apps\AppProvider;
+use BeyondCode\LaravelWebSockets\Dashboard\DashboardLogger;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +12,27 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (AppProvider $appProvider) {
+    return view('chat-app-example', [
+        'port' => '6001',
+        'host' => '127.0.0.1',
+        'authEndpoint' => '/api/sockets/connect',
+        'logChannel' => DashboardLogger::LOG_CHANNEL_PREFIX,
+        'apps' => $appProvider->all(),
+    ]);
+});
+
+Route::post('/chat/send', function (Request $request) {
+    $message = $request->input('message', null);
+    $name = $request->input('name', 'Anonymous');
+    $time = (new DateTime(now()))->format(DateTime::ATOM);
+    if ($name == null) {
+        $name = 'Anonymous';
+    }
+    SendMessage::dispatch($name, $message, $time);
 });
